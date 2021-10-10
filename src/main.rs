5f9b91bf3c8bad135758c197c9c95d992fbaee53
Nick <speedyleion@users.noreply.github.com>
2021-10-09 19:16:15 -0700

    adding in the basic druid example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
+use druid::widget::Label;
+
+fn build_ui() -> impl Widget<()> {
+    Label::new("Hello world")
+}
+
+fn main() -> Result<(), PlatformError> {
+    let window = WindowDesc::new(build_ui()).title("Literate Broccoli");
+    AppLauncher::with_window(window).launch(())?;
+    Ok(())
 }
