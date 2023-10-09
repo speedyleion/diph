c69ea320faaf3b4085735e94368d00ae40df1ca1
Nick <speedyleion@users.noreply.github.com>
2023-10-08 19:56:34 -0700

    A basic window using makepad

---
+++ src/app.rs
@@ -0,0 +1,48 @@
+//          Copyright Nick G 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+use makepad_widgets::*;
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+
+    App = {{App}} {
+        ui: <Window>{
+            show_bg: true
+            width: Fill,
+            height: Fill
+
+            body = {dock = <Dock> {
+                height: Fill,
+                width: Fill
+                FileTree = <FileTree> {}
+            }}
+        }
+    }
+}
+
+#[derive(Live)]
+pub struct App {
+    #[live]
+    ui: WidgetRef,
+}
+
+app_main!(App);
+
+impl LiveHook for App {
+    fn before_live_design(cx: &mut Cx) {
+        makepad_widgets::live_design(cx);
+    }
+}
+
+impl AppMain for App{
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        if let Event::Draw(event) = event {
+            return self.ui.draw_widget_all(&mut Cx2d::new(cx, event));
+        }
+
+        let _ = self.ui.handle_widget_event(cx, event);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,4 @@
 //    (See accompanying file LICENSE or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-fn main(){
-    diph::app::app_main()
-}
+pub mod app;
