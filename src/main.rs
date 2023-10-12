13af1d0263fdd431dfe5c341a308f964cf0e75fa
Nick <speedyleion@users.noreply.github.com>
2023-10-11 18:13:56 -0700

    Reset again
    I gave it a go to try and use makepad, but I think I'm going to spend
    more time trying to figure out the framework than actually writing the
    meaningful code, punting for now to another ui framework.

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-//          Copyright Nick G 2023.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE or copy at
-//          https://www.boost.org/LICENSE_1_0.txt)
-
-fn main(){
-    diph::app::app_main()
-}
